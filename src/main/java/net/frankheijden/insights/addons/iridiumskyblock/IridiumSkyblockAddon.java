2c4303a3b7ab1e9210bae2978a365849c877ceef
Frank van der Heijden <frank.boekanier@gmail.com>
2020-12-27 15:01:18 +0100

    v1.1.0 - Conform to new Insights assistant update

--- src/main/java/net/frankheijden/insights/addons/iridiumskyblock/IridiumSkyblockAddon.java
+++ src/main/java/net/frankheijden/insights/addons/iridiumskyblock/IridiumSkyblockAddon.java
@@ -2,27 +2,34 @@ package net.frankheijden.insights.addons.iridiumskyblock;
 
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.Island;
+import net.frankheijden.insights.entities.Area;
 import net.frankheijden.insights.entities.CacheAssistant;
-import net.frankheijden.insights.entities.Selection;
+import net.frankheijden.insights.entities.CuboidSelection;
 import org.bukkit.Location;
+
+import java.util.Collections;
 
 public class IridiumSkyblockAddon extends CacheAssistant {
 
     public IridiumSkyblockAddon() {
-        super("IridiumSkyblock", "IridiumSkyblock", "island", "1.0.1");
+        super("IridiumSkyblock", "IridiumSkyblock", "island", "1.1.0");
     }
 
-    public Selection adapt(Island island) {
+    public String getId(Island island) {
+        return getPluginName() + "@" + island.getId();
+    }
+
+    public Area adapt(Island island) {
         if (island == null) return null;
         Location min = island.getPos1().clone();
         min.setY(0);
         Location max = island.getPos2().clone();
         max.setY(max.getWorld().getMaxHeight() - 1);
-        return new Selection(min, max);
+        return Area.from(this, getId(island), Collections.singletonList(new CuboidSelection(min, max)));
     }
 
     @Override
-    public Selection getSelection(Location location) {
+    public Area getArea(Location location) {
         return adapt(IridiumSkyblock.getIslandManager().getIslandViaLocation(location));
     }
 }
